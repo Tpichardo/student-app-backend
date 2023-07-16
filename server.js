f75a821266577dfdeb2e0a1cc2879c466961c0a2
Talia Pichardo <taliapichardo@gmail.com>
2023-07-16 15:45:23 -0400

    create server

---
+++ server.js
@@ -0,0 +1,12 @@
+//Import the app
+//Start up and listen
+
+require('dotenv').config();
+
+const app = require('./app');
+
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, () =>{
+    console.log(`Listening on port ${PORT}`)
+})
