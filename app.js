e1b6ad2cf94e4098c7be1605790a86601aaf9697
Talia Pichardo <taliapichardo@gmail.com>
2023-07-16 16:34:27 -0400

    Add /students handler function

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 
+const studentData = require('./studentData.json');
+
 //create an instance of an express application
 //when this function is called it creates an express application and asigns it to the var app
 const app = express();
@@ -12,7 +14,18 @@ const app = express();
 
 app.get('/', (request, response) => {
     response.status(200).json({ data: "Service is running"});
+});
 
+// GET/students
+//define path + method and handler
+//catch errros
+app.get('/students', (request, response) => {
+    try {
+        const { students } = studentData;
+        response.status(200).json({ data: students })
+    } catch (err) {
+        response.status(500).json({ error : err.message})
+    }
 });
 
 module.exports = app;
